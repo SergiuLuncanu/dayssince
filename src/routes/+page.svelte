a7b45d3a1d1c2f6e2f3cae7586a8da06742cc4b9
Sebastian Olaru <sebastianolaru3008@gmail.com>
2023-05-16 14:03:32 +0300

    feat: redesigned the chalkboard for days since pump died

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,100 @@
-
-<svelte:head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&family=Permanent+Marker&display=swap" rel="stylesheet">
-    <title>
-        Days Since Last Pump Died
-    </title>
-</svelte:head>
-
-
 <script>
     import { onMount } from "svelte";
 
     let diff = 0;
-    
-    function getDaysDifference(date1, date2){
-        const diffTime = Math.abs(date2 -date1);
-        return Math.ceil(diffTime / (60 * 60 * 24 * 1000))
+
+    function getDaysDifference(date1, date2) {
+        const diffTime = Math.abs(date2 - date1);
+        return Math.ceil(diffTime / (60 * 60 * 24 * 1000));
     }
-    
+
     onMount(() => {
-        let lastPumpDiedOn = new Date('5/11/2023')
-        let currentDate = new Date()
-        
+        let lastPumpDiedOn = new Date("5/11/2021");
+        let currentDate = new Date();
+
         diff = getDaysDifference(currentDate, lastPumpDiedOn);
-
     });
-
 </script>
 
-<div class="container">
-    <span class="text-title">Days since last pump died </span>
-    <span class="text-days">{diff}</span>
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@400;700&family=Londrina+Sketch&display=swap"
+        rel="stylesheet"
+    />
+    <link rel="stylesheet" href="/reset.css" />
+    <title>Days Since Last Pump Died</title>
+</svelte:head>
+
+<div class="chalkboard-region">
+    <div class="chalkboard-pin" />
+    <div class="chalkboard-hanger" />
+    <div class="chalkboard">
+        <span class="text-title">Days since last pump died </span>
+        <span class="text-days">{diff}</span>
+    </div>
 </div>
 
-
-
 <style>
+    .text-title {
+        font-size: 64px;
+        text-align: center;
+        margin: 20px;
+        color: #ffffff;
+    }
 
-.text-title{
-    font-size: 48px;
-    margin: 20px;
-    color: #084a84;
-}
+    .text-days {
+        font-size: 96px;
+        font-weight: bold;
+        color: #ffbf00;
+        font-family: "Cabin Sketch", cursive;
+    }
 
-.text-days{
-    font-size: 48px;
-    color: #fdd10b;
-    font-family: 'Permanent Marker', cursive;
-}
+    :global(body) {
+        min-height: 100vh;
+        display: grid;
+        justify-content: center;
+    }
 
-:global(body){
-    height: 100%;
-    margin: 0;
-    padding: 0;
-}
+    .chalkboard-region {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: min-content;
+        /* outline: red solid 1px; */
+    }
 
-.container{
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-top: 100px;
-    margin: 0;
-    font-family: 'Open Sans', sans-serif;
-}
+    .chalkboard-pin {
+        position: absolute;
+        background-color: #ffbf00;
+        border-radius: 50%;
+        height: 16px;
+        width: 16px;
+        z-index: 1;
+    }
 
+    .chalkboard-hanger {
+        position: absolute;
+        border: 3px solid;
+        border-color: grey transparent transparent grey;
+        height: 150px;
+        width: 150px;
+        transform: rotateZ(45deg) translate(15%, 15%);
+    }
+
+    .chalkboard {
+        background-color: black;
+        border: solid 10px;
+        border-color: #964b00 #6d3700 #482400 #7c3e00;
+        max-width: 32em;
+        max-height: 28em;
+        margin-top: 106px;
+        display: flex;
+        flex-direction: column;
+        flex-shrink: inherit;
+        align-items: center;
+        justify-content: center;
+        font-family: "Cabin Sketch", sans-serif;
+    }
 </style>
